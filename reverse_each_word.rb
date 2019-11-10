aa0a3f5f3a577a249e9a5aac3577a00e595d3888
Sean Good <tree662@protonmail.com>
2019-11-10 22:28:27 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,33 @@
+# def reverse_each_word(string)
+#   words = string.split(" ") #splitting string into an Array of words
+# 	new_sentence = []
+# 	words.each do |word|
+# 		# doing the same thing as reverse_whole_string
+# 		# but with each word from an array
+# 		end_loop = word.length
+# 		new_string = ""
+# 		while end_loop > 0
+# 			end_loop -= 1
+# 			new_string += word[end_loop] #Reversing every letter of each word
+# 		end
+# 		new_sentence << new_string # appending every word to an new Array 
+# 		end
+# 	new_sentence.join(" ") #joining Array into a string delimited by spaces
+# end
+
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.each do |word|
+    
+  end 
+  array.join(" ")
+end
+
+def uses_collect(string)
+  array = string.split(" ")
+  array.collect do |word|
+    word.reverse! 
+  end
+  array.join(" ")
+end
